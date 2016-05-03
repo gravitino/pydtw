80c6940703b5f9356ffa4b67a3553820c28a62d6
Christian Hundt <christian@metalabs.de>
2016-05-03 14:11:53 +0200

    DTW prototype

--- pydtw/source/include/elastic.hpp
+++ pydtw/source/include/elastic.hpp
@@ -30,8 +30,7 @@
 // includes
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <vector>
-#define PYDTW_CONSTANTS_INFINITY (9999999)
+#include "constants.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 // elastic measures
@@ -40,7 +39,8 @@
 template <
     typename index_t,
     typename value_t,
-    typename funct_t>
+    typename funct_t,
+    bool compute_backtrace=false>
 value_t elastic_dtw(
     value_t * series0,
     index_t   length0,
@@ -48,11 +48,17 @@ value_t elastic_dtw(
     index_t   length1,
     funct_t   metric) {
 
-    // convenience
+    // convenience variables
     const index_t lane_i = length0+1;
     const index_t lane_j = length1+1;
     const index_t area = lane_i*lane_j;
-    std::vector<value_t> penalty(area);
+
+    value_t * penalty = new value_t[area];
+    uint8_t * predecs = nullptr;
+
+    // backtracing needs quadratic memory: suppress if not needed
+    if (compute_backtrace)
+        predec = new uint8_t[area];
 
     // initialize penalty matrix
     for (index_t j = 1; j < lane_j; j++)
@@ -61,12 +67,52 @@ value_t elastic_dtw(
         penalty[i*lane_j+0] = PYDTW_CONSTANTS_INFINITY;
     penalty[0] = 0;
 
-    for (index_t i = 0; i < lane_i; i++)
-        for (index_t j = 0; j < lane_j; j++) {
-            const value_t residue = metric(series0, series1);
-        }
+    // start relaxing
+    for (index_t i = 1; i < lane_i; i++) {
 
-    return penalty[area-1];
+        // compute bounds for the window
+        const index_t lower = 1;
+        const index_t upper = lane_j;
+
+        for (index_t j = lower; j < upper; j++) {
+
+            // compute local measure
+            const value_t cost = metric(series0+(i-1)*metric.stride,
+                                        series1+(j-1)*metric.stride);
+
+            // cache matrix entries
+            const diag = penalty[(i-1)*lane_j+(j-1)];
+            const abve = penalty[(i-1)*lane_j+(j+0)];
+            const left = penalty[(i-0)*lane_j+(j-1)];
+
+            // prefer diagonal steps if tied
+            value_t bsf_value = diag;
+            index_t bsf_index = PYDTW_CONSTANTS_DIAGONAL;
+
+            // search best extension of the warping path
+            if (bsf_value > abve) {
+                bsf_value = abve;
+                if (compute_backtrace)
+                    bsf_index = PYDTW_CONSTANTS_ABOVE;
+            }
+            if (bsf_value > left) {
+                bsf_value = left;
+                if (compute_backtrace)
+                    bsf_index = PYDTW_CONSTANTS_LEFT;
+            }
+
+            // relax cell
+            penalty[i*lane_j+j] = cost + bsf_value;
+        }
+    }
+
+    const value_t result = penalty[area-1];
+    delete [] penalty;
+
+    if (compute_backtrace)
+        delete [] predecs;
+
+    return result;
 }
 
 #endif
